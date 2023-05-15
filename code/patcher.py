d411aa47bb7a2969cc874a8b8b29346ff93be951
Elisha Rosensweig <elisha@verbit.ai>
2023-05-15 11:25:26 +0300

    latest

--- code/patcher.py
+++ code/patcher.py
@@ -1,7 +1,8 @@
 import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
-
+import torchvision.transforms as transforms
+import numpy as np
 
 import patcher_utils as putils
 
@@ -12,26 +13,30 @@ class Patcher(object):
         self.save_path = save_path
         self.shape = patch_params['shape']
         self.size = patch_params['size']
-        self.dims = patch_params['dimensions']
         self.image_size = patch_params['image_size']
 
         if self.shape == 'circle':
             self.init_func = putils.init_patch_circle
-            self.trans_func = putils.circle_transform
+            self.trans_func = putils.circle_rotate
         elif self.shape == 'square':
             self.init_func = putils.init_patch_square
-            self.trans_func = putils.square_transform
+            self.trans_func = putils.square_rotate
 
-        self.patch = self.init_func(image_size=self.image_size, patch_size=self.size)
+        self.patch, self.dims = self.init_func(image_size=self.image_size, patch_size=self.size)
 
-    def patch_image(self, image, randomize=True):
+    def prepare_patch(self, image):
         """
-        given an image, patch it at a random place & orientation.
-        if randomize = True, make random modifications to patch
+        given an image, prepare a patch for it it at a random place & orientation.
         """
-        self.trans_func(patch=self.patch, patch_shape=self.dims, image_size=self.image_size, )
 
-        return torch.mul((1 - mask), image) + torch.mul(mask, self.patch)
+        # todo: should there be a distinction b/w patch and patch_slide? I converted all the variables here to slide,
+        # todo: but is that correct?
+        image_shape = image.data.cpu().numpy().shape
+        patch_slide, mask = putils.patch_transform(patcher=self.patch, data_shape=image_shape,
+                                                   rotate_func=self.trans_func)
+        patch_slide, mask = torch.FloatTensor(patch_slide), torch.FloatTensor(mask)
+        patch_slide, mask = Variable(patch_slide), Variable(mask)
+        return torch.mul((1 - mask), image) + torch.mul(mask, patch_slide), mask
 
     def save_patch_to_disk(self):
         """
@@ -42,91 +47,107 @@ class Patcher(object):
     def get_patch(self):
         return self.patch
 
-    def modify_patch(self, params):
+    def update_patch(self, patch):
         """
         modify patch as defined by params
         """
-        pass
+        self.patch = patch
+
+
+
 
 
 class PatchAttacker(object):
 
-    def __init__(self, classifier, attack_params):
+    def __init__(self, classifier, attack_params, train_size, test_size):
         self.classifier = classifier
         self.params = attack_params
         self.patcher = None
-        self.attack_stats = None
+        self.attack_stats = dict()
         self.test_stats = None
-        self.test_set = None
-        self.train_set = None
-        self._init()
+        self.ignore_idx = set()  # list of image ids to ignore for training
+        self._init_datasets(train_size, test_size)
 
-    def _init(self):
-        # sample test and train sets
-        pass
+    def _init_datasets(self, train_size, test_size):
+        print("==> initializing datasets")
 
-    def attack_classifier(self, target):
+        idx = np.arange(50000)  # number of images in validation set of imagenet
+        np.random.shuffle(idx)
+        training_idx = idx[:train_size]
+        test_idx = idx[train_size:test_size]
+        normalize = transforms.Normalize(mean=self.classifier.mean, std=self.classifier.std)
+        self.train_set = putils.get_data_loader(classifier=self.classifier, num_workers=2,
+                                                idx=training_idx, normalize=normalize)
+        self.test_set = putils.get_data_loader(classifier=self.classifier, num_workers=2,
+                                               idx=test_idx, normalize=normalize)
+
+        min_in, max_in = self.classifier.input_range[0], self.classifier.input_range[1]
+        min_in, max_in = np.array([min_in, min_in, min_in]), np.array([max_in, max_in, max_in])
+        mean, std = np.array(self.classifier.mean), np.array(self.classifier.std)
+        self.min_out, self.max_out = np.min((min_in - mean) / std), np.max((max_in - mean) / std)
+        print("==> initialized datasets !!")
+
+
+    def train_patch(self, target):
         # 1. get starting target image from classifier
         # 2. get image for class and init patcher object
         self.patcher = Patcher()
         # 3. for the number of epochs, run attacks
         for i in range(self.params['epochs']):
             print(f"Running Training Epoch {i}")
-            self._attack_epoch(target)
+            total, success = self._train_epoch(target)
+            self.attack_stats[i] = (total, success)
         return self.patcher, self.attack_stats
 
-    def _attack_epoch(self, target):
+    def _train_epoch(self, target):
         # 1. sample images from image library
         # 2. for each image patch the image
-        for image in self.train_set:
-            patched_image = self.patcher.patch_image(image=image)
+        success = 0
+        total = 0
+        for image_idx, (image, labels) in enumerate(self.train_set):
+            image, labels = Variable(image), Variable(labels)
+            if image_idx in self.ignore_idx or self._misclassified(image, labels):
+                self.ignore_idx.add(image_idx)
+                continue
+            total += 1
+            patch, mask = self.patcher.prepare_patch(image)
+            patched_image, new_patch = self._adapt_patch_to_image(victim=image, target=target, patch=patch, mask=mask)
+            self.patcher.update_patch(new_patch)
+
             # 3. apply classifier to patched
             prediction = self._predict(patched_image)
-            # 4. compute loss
-            # 5. modify patch
-            modify_params = {}
-            self.patcher.modify_patch(params=modify_params)
+            if prediction == target:
+                success += 1
+        return total, success
 
+    def _misclassified(self, image, labels):
+        return self._predict(image) != labels.data[0]
 
-    def _attack_image(self, victim, target, mask):
+    def _adapt_patch_to_image(self, victim, target, patch, mask):
         self.classifier.eval()
-
         smax = F.softmax(self.classifier(victim))
-        target_prob = smax.data[0][target]
-
-        patch = self.patcher.get_patch()
-        adv_x = torch.mul((1 - mask), victim) + torch.mul(mask, patch)
+        target_prob = smax.data[0][target]  # current state of attack
+        adv_image = torch.mul((1 - mask), victim) + torch.mul(mask, patch)
 
         count = 0
 
-        while self.params['conf_target'] > self.params['target_prob'] \
-                and count < self.params['max_iter']:
-            adv_x = Variable(adv_x.data, requires_grad=True)
-            adv_out = F.log_softmax(netClassifier(adv_x))
-
-            adv_out_probs, adv_out_labels = adv_out.max(1)
+        while self.params['conf_target'] > target_prob  and count < self.params['max_iter']:
+            adv_image = Variable(adv_image.data, requires_grad=True)
+            adv_out = F.log_softmax(self.classifier(adv_image))
 
             Loss = -adv_out[0][target]
             Loss.backward()
-
-            adv_grad = adv_x.grad.clone()
-
-            adv_x.grad.data.zero_()
-
+            adv_grad = adv_image.grad.clone()
+            adv_image.grad.data.zero_()
             patch -= adv_grad
+            adv_image = torch.mul((1 - mask), victim) + torch.mul(mask, patch)
+            adv_image = torch.clamp(adv_image, self.min_out, self.max_out)
 
-            adv_x = torch.mul((1 - mask), x) + torch.mul(mask, patch)
-            adv_x = torch.clamp(adv_x, min_out, max_out)
-
-            out = F.softmax(netClassifier(adv_x))
+            out = F.softmax(self.classifier(adv_image))
             target_prob = out.data[0][target]
-            # y_argmax_prob = out.data.max(1)[0][0]
-
-            # print(count, conf_target, target_prob, y_argmax_prob)
-
             count += 1
 
-        return adv_x, mask, patch
+        return adv_image, patch
 
     def _predict(self, data):
         return self.classifier(data).data.max(1)[1][0]
--- code/patcher_utils.py
+++ code/patcher_utils.py
@@ -4,6 +4,13 @@ import time
 import math
 import numpy as np
 from scipy.ndimage.interpolation import rotate
+import torch
+import torch.nn.parallel
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+from torch.utils.data.sampler import SubsetRandomSampler
+
 
 _, term_width = os.popen('stty size', 'r').read().split()
 term_width = int(term_width)
@@ -101,6 +108,7 @@ def submatrix(arr):
 class ToSpaceBGR(object):
     def __init__(self, is_bgr):
         self.is_bgr = is_bgr
+
     def __call__(self, tensor):
         if self.is_bgr:
             new_tensor = tensor.clone()
@@ -113,6 +121,7 @@ class ToSpaceBGR(object):
 class ToRange255(object):
     def __init__(self, is_255):
         self.is_255 = is_255
+
     def __call__(self, tensor):
         if self.is_255:
             tensor.mul_(255)
@@ -126,49 +135,60 @@ def init_patch_circle(image_size, patch_size):
     patch = np.zeros((1, 3, radius*2, radius*2))    
     for i in range(3):
         a = np.zeros((radius*2, radius*2))    
-        cx, cy = radius, radius # The center of circle 
+        cx, cy = radius, radius  # The center of circle
         y, x = np.ogrid[-radius: radius, -radius: radius]
         index = x**2 + y**2 <= radius**2
         a[cy-radius:cy+radius, cx-radius:cx+radius][index] = np.random.rand()
         idx = np.flatnonzero((a == 0).all((1)))
         a = np.delete(a, idx, axis=0)
         patch[0][i] = np.delete(a, idx, axis=1)
+    return patch, patch.shape
+
+
+def square_rotate(patch, i):
+    rot = np.random.choice(4)
+    for j in range(patch[i].shape[0]):
+        patch[i][j] = np.rot90(patch[i][j], rot)
     return patch
 
 
-def circle_transform(patch, data_shape, patch_shape, image_size):
-    # get dummy image 
-    x = np.zeros(data_shape)
-   
-    # get shape
-    m_size = patch_shape[-1]
-    
-    for i in range(x.shape[0]):
+def circle_rotate(patch, i):
+    rot = np.random.choice(360)
+    for j in range(patch[i].shape[0]):
+        patch[i][j] = rotate(patch[i][j], angle=rot, reshape=False)
+    return patch
 
-        # random rotation
-        rot = np.random.choice(360)
-        for j in range(patch[i].shape[0]):
-            patch[i][j] = rotate(patch[i][j], angle=rot, reshape=False)
-        
+
+# todo: can this logic be true? it seems that they are positioning
+# todo: each bit in it's own random location. Why???
+def patch_transform(patcher, data_shape, rotate_func):
+    # get dummy image
+    patch_slide = np.zeros(data_shape)
+    patch = patcher.patch
+
+    # get shape
+    m_size = patcher.dims[-1]
+
+    for i in range(patch_slide.shape[0]):
+
+        patch = rotate_func(patch, i)
+
         # random location
-        random_x = np.random.choice(image_size)
-        if random_x + m_size > x.shape[-1]:
-            while random_x + m_size > x.shape[-1]:
-                random_x = np.random.choice(image_size)
-        random_y = np.random.choice(image_size)
-        if random_y + m_size > x.shape[-1]:
-            while random_y + m_size > x.shape[-1]:
-                random_y = np.random.choice(image_size)
-       
-        # apply patch to dummy image  
-        x[i][0][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[i][0]
-        x[i][1][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[i][1]
-        x[i][2][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[i][2]
-    
-    mask = np.copy(x)
+        random_x = np.random.choice(patcher.image_size)
+        while random_x + m_size > patch_slide.shape[-1]:
+            random_x = np.random.choice(patcher.image_size)
+        random_y = np.random.choice(patcher.image_size)
+        while random_y + m_size > patch_slide.shape[-1]:
+            random_y = np.random.choice(patcher.image_size)
+
+        # apply patch to dummy image
+        for j in range(3):
+            patch_slide[i][j][random_x:random_x + m_size, random_y:random_y + m_size] = patch[i][j]
+
+    mask = np.copy(patch_slide)
     mask[mask != 0] = 1.0
-    
-    return x, mask, patch.shape
+
+    return patch_slide, mask
 
 
 def init_patch_square(image_size, patch_size):
@@ -177,40 +197,19 @@ def init_patch_square(image_size, patch_size):
     noise_size = image_size*patch_size
     noise_dim = int(noise_size**(0.5))
     patch = np.random.rand(1, 3, noise_dim, noise_dim)
-    return patch
+    return patch, patch.shape
 
 
-def square_transform(patch, data_shape, patch_shape, image_size):
-    # get dummy image 
-    x = np.zeros(data_shape)
-    
-    # get shape
-    m_size = patch_shape[-1]
-    
-    for i in range(x.shape[0]):
-
-        # random rotation
-        rot = np.random.choice(4)
-        for j in range(patch[i].shape[0]):
-            patch[i][j] = np.rot90(patch[i][j], rot)
-        
-        # random location
-        random_x = np.random.choice(image_size)
-        if random_x + m_size > x.shape[-1]:
-            while random_x + m_size > x.shape[-1]:
-                random_x = np.random.choice(image_size)
-        random_y = np.random.choice(image_size)
-        if random_y + m_size > x.shape[-1]:
-            while random_y + m_size > x.shape[-1]:
-                random_y = np.random.choice(image_size)
-       
-        # apply patch to dummy image  
-        x[i][0][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[i][0]
-        x[i][1][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[i][1]
-        x[i][2][random_x:random_x+patch_shape[-1], random_y:random_y+patch_shape[-1]] = patch[i][2]
-    
-    mask = np.copy(x)
-    mask[mask != 0] = 1.0
-    
-    return x, mask, patch_shape
+def get_data_loader(classifier, num_workers, idx, normalize):
+    return torch.utils.data.DataLoader(
+        dset.ImageFolder('./imagenetdata/val', transforms.Compose([
+            transforms.Resize(round(max(classifier.input_size) * 1.050)),
+            transforms.CenterCrop(max(classifier.input_size)),
+            transforms.ToTensor(),
+            ToSpaceBGR(classifier.input_space == 'BGR'),
+            ToRange255(max(classifier.input_range) == 255),
+            normalize,
+        ])),
+        batch_size=1, shuffle=False, sampler=SubsetRandomSampler(idx),
+        num_workers=num_workers, pin_memory=True)
 
